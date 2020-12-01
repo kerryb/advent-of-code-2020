946e0716b8ce9edb0048085ab63b5e7fed3b64bc
Kerry Buckley <kerryjbuckley@gmail.com>
2020-12-01 23:21:11 +0000

    Add test

--- report_repair/test/report_repair_test.exs
+++ report_repair/test/report_repair_test.exs
@@ -1,4 +1,17 @@
 defmodule ReportRepairTest do
   use ExUnit.Case
   doctest ReportRepair
+
+  test "finds and multiplies the two numbers which total 2020" do
+    input = """
+            1721
+            979
+            366
+            299
+            675
+            1456
+            """
+
+    assert ReportRepair.find(input) == 514_579
+  end
 end
