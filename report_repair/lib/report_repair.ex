23049dfbdf12bb385799ce54a43b54a05622be40
Kerry Buckley <kerryjbuckley@gmail.com>
2020-12-02 17:46:42 +0000

    Day one part two

--- report_repair/lib/report_repair.ex
+++ report_repair/lib/report_repair.ex
@@ -1,19 +1,37 @@
 defmodule ReportRepair do
-  def find(input) do
-    numbers =
-      input
-      |> String.split("\n", trim: true)
-      |> Enum.map(&String.to_integer/1)
+  def find_pair(input) do
+    numbers = parse(input)
 
     numbers
     |> Enum.find_value(&find_pair(&1, numbers))
     |> Enum.reduce(&*/2)
   end
 
-  defp find_pair(value, numbers) do
-    case Enum.find(numbers, &(&1 + value == 2020)) do
+  def find_trio(input) do
+    numbers = parse(input)
+
+    numbers
+    |> Enum.find_value(&find_trio(&1, numbers))
+    |> Enum.reduce(&*/2)
+  end
+
+  defp parse(input) do
+    input
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  defp find_pair(value, numbers, total \\ 2020) do
+    case Enum.find(numbers, &(&1 + value == total)) do
       nil -> nil
       number -> [value, number]
+    end
+  end
+
+  defp find_trio(value, numbers) do
+    case Enum.find_value(numbers, &find_pair(&1, numbers, 2020 - value)) do
+      nil -> nil
+      [a, b] -> [value, a, b]
     end
   end
 end
--- report_repair/test/report_repair_test.exs
+++ report_repair/test/report_repair_test.exs
@@ -2,16 +2,33 @@ defmodule ReportRepairTest do
   use ExUnit.Case
   doctest ReportRepair
 
-  test "finds and multiplies the two numbers which total 2020" do
-    input = """
-            1721
-            979
-            366
-            299
-            675
-            1456
-            """
+  describe "ReportRepair.find_pair/1" do
+    test "finds and multiplies the two numbers which total 2020" do
+      input = """
+      1721
+      979
+      366
+      299
+      675
+      1456
+      """
 
-    assert ReportRepair.find(input) == 514_579
+      assert ReportRepair.find_pair(input) == 514_579
+    end
+  end
+
+  describe "ReportRepair.find_trio/1" do
+    test "finds and multiplies the three numbers which total 2020" do
+      input = """
+      1721
+      979
+      366
+      299
+      675
+      1456
+      """
+
+      assert ReportRepair.find_trio(input) == 241_861_950
+    end
   end
 end
