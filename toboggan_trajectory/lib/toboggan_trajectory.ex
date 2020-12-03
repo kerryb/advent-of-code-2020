b38f8e7bdc98ab6bf239593c1ab6c7baa6d7ade8
Kerry Buckley <kerryjbuckley@gmail.com>
2020-12-03 13:52:30 +0000

    Day three (forgot to commit part one)

---
+++ toboggan_trajectory/lib/toboggan_trajectory.ex
@@ -0,0 +1,58 @@
+defmodule TobogganTrajectory do
+  @doc ~S'''
+  iex> input = """
+  ...> ..##.......
+  ...> #...#...#..
+  ...> .#....#..#.
+  ...> ..#.#...#.#
+  ...> .#...##..#.
+  ...> ..#.##.....
+  ...> .#.#.#....#
+  ...> .#........#
+  ...> #.##...#...
+  ...> #...##....#
+  ...> .#..#...#.#
+  ...> """
+  ...> TobogganTrajectory.count_trees(input, 1, 1)
+  2
+  iex> TobogganTrajectory.count_trees(input, 3, 1)
+  7
+  iex> TobogganTrajectory.count_trees(input, 5, 1)
+  3
+  iex> TobogganTrajectory.count_trees(input, 7, 1)
+  4
+  iex> TobogganTrajectory.count_trees(input, 1, 2)
+  2
+  '''
+  def count_trees(input, right, down) do
+    input
+    |> parse()
+    |> travel(right, down, 0)
+  end
+
+  def count_trees_for_multiple_paths(input) do
+    for {right, down} <- [{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}] do
+      count_trees(input, right, down)
+    end
+    |> Enum.reduce(&*/2)
+  end
+
+  defp parse(input), do: String.split(input, "\n", trim: true)
+
+  defp travel(rows, right, down, index) do
+    case Enum.drop(rows, down) do
+      [] ->
+        0
+
+      remaining_rows ->
+        new_index = index + right
+
+        if(tree?(remaining_rows, new_index), do: 1, else: 0) +
+          travel(remaining_rows, right, down, new_index)
+    end
+  end
+
+  defp tree?([row | _], index) do
+    String.at(row, Integer.mod(index, String.length(row))) == "#"
+  end
+end
---
+++ toboggan_trajectory/test/toboggan_trajectory_test.exs
@@ -0,0 +1,4 @@
+defmodule TobogganTrajectoryTest do
+  use ExUnit.Case
+  doctest TobogganTrajectory
+end
