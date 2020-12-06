0fc86418af6827c2af601d1d0a8592edd6f0cf19
Kerry Buckley <kerryjbuckley@gmail.com>
2020-12-06 10:10:03 +0000

    Day six part two

--- custom_customs/lib/custom_customs.ex
+++ custom_customs/lib/custom_customs.ex
@@ -21,19 +21,56 @@ defmodule CustomCustoms do
       ...>
       ...> b
       ...> """
-      ...> CustomCustoms.count(input)
+      ...> CustomCustoms.count_any(input)
       11
   '''
-  def count(input) do
+  def count_any(input) do
     input
     |> parse()
     |> count_group_answers()
     |> Enum.sum()
   end
 
+  @doc ~S'''
+      iex> input = """
+      ...> abc
+      ...>
+      ...> a
+      ...> b
+      ...> c
+      ...>
+      ...> ab
+      ...> ac
+      ...>
+      ...> a
+      ...> a
+      ...> a
+      ...> a
+      ...>
+      ...> b
+      ...> """
+      ...> CustomCustoms.count_all(input)
+      6
+  '''
+  def count_all(input) do
+    input
+    |> parse()
+    |> parse_groups()
+    |> count_group_common_answers()
+    |> Enum.sum()
+  end
+
   defp parse(input) do
     input
     |> String.split("\n\n")
+  end
+
+  defp parse_groups(groups), do: Enum.map(groups, &parse_group/1)
+
+  defp parse_group(group) do
+    group
+    |> String.split("\n", trim: true)
+    |> Enum.map(&String.graphemes/1)
   end
 
   defp count_group_answers(answers), do: Enum.map(answers, &count_unique/1)
@@ -43,5 +80,17 @@ defmodule CustomCustoms do
     |> String.graphemes()
     |> Enum.uniq()
     |> Enum.count(&(&1 != "\n"))
+  end
+
+  defp count_group_common_answers(answers), do: Enum.map(answers, &count_common/1)
+
+  defp count_common(group) do
+    group
+    |> Enum.reduce(&common_answers/2)
+    |> Enum.count()
+  end
+
+  defp common_answers(a, b) do
+    Enum.filter(a, &(&1 in b))
   end
 end
