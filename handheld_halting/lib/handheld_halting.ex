a916829b29c683c6d958e37f18c3958a91e1e01c
Kerry Buckley <kerryjbuckley@gmail.com>
2020-12-08 14:43:30 +0000

    Day eight part two

--- handheld_halting/lib/handheld_halting.ex
+++ handheld_halting/lib/handheld_halting.ex
@@ -22,6 +22,30 @@ defmodule HandheldHalting do
     input
     |> parse()
     |> execute_until_repeat()
+    |> elem(1)
+  end
+
+  @doc ~S'''
+      iex> input = """
+      ...> nop +0
+      ...> acc +1
+      ...> jmp +4
+      ...> acc +3
+      ...> jmp -3
+      ...> acc -99
+      ...> acc +1
+      ...> jmp -4
+      ...> acc +6
+      ...> """
+      ...> HandheldHalting.fix_and_run(input)
+      8
+  '''
+  def fix_and_run(input) do
+    program = parse(input)
+
+    program
+    |> Map.keys()
+    |> Enum.find_value(&fix_at_index(&1, program))
   end
 
   defp parse(input) do
@@ -37,8 +61,11 @@ defmodule HandheldHalting do
 
   defp execute_until_repeat(program, pointer \\ 0, acc \\ 0) do
     case program[pointer] do
+      nil ->
+        {:finished, acc}
+
       %{executed?: true} ->
-        acc
+        {:loop, acc}
 
       %{instruction: "nop"} ->
         execute_until_repeat(put_in(program[pointer].executed?, true), pointer + 1, acc)
@@ -52,6 +79,19 @@ defmodule HandheldHalting do
 
       %{instruction: "jmp", argument: argument} ->
         execute_until_repeat(put_in(program[pointer].executed?, true), pointer + argument, acc)
+    end
+  end
+
+  defp fix_at_index(index, program) do
+    program[index]
+    |> case do
+      %{instruction: "nop"} -> execute_until_repeat(put_in(program[index].instruction, "jmp"))
+      %{instruction: "jmp"} -> execute_until_repeat(put_in(program[index].instruction, "nop"))
+      _ -> false
+    end
+    |> case do
+      {:finished, acc} -> acc
+      _ -> false
     end
   end
 end
