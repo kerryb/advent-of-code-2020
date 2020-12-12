c1b725a7b8b83af6d9141cd91370e5f414694997
Kerry Buckley <kerryjbuckley@gmail.com>
2020-12-12 19:01:04 +0000

    Day 12 part two

--- 12_rain_risk/lib/rain_risk.ex
+++ 12_rain_risk/lib/rain_risk.ex
@@ -21,6 +21,24 @@ defmodule RainRisk do
     |> manhattan_distance()
   end
 
+  @doc ~S'''
+      iex> input = """
+      ...> F10
+      ...> N3
+      ...> F7
+      ...> R90
+      ...> F11
+      ...> """
+      ...> RainRisk.navigate_by_waypoint(input)
+      286
+  '''
+  def navigate_by_waypoint(input) do
+    input
+    |> parse()
+    |> follow_waypoint()
+    |> manhattan_distance()
+  end
+
   defp parse(input) do
     ~r/^(.)(\d+)/m
     |> Regex.scan(input, capture: :all_but_first)
@@ -54,20 +72,27 @@ defmodule RainRisk do
   defp move({"R", 270}, {x, y, "S"}), do: {x, y, "E"}
   defp move({"R", 270}, {x, y, "W"}), do: {x, y, "S"}
 
-  defp move({"L", 90}, {x, y, "N"}), do: {x, y, "W"}
-  defp move({"L", 90}, {x, y, "E"}), do: {x, y, "N"}
-  defp move({"L", 90}, {x, y, "S"}), do: {x, y, "E"}
-  defp move({"L", 90}, {x, y, "W"}), do: {x, y, "S"}
+  defp move({"L", value}, state), do: move({"R", 360 - value}, state)
 
-  defp move({"L", 180}, {x, y, "N"}), do: {x, y, "S"}
-  defp move({"L", 180}, {x, y, "E"}), do: {x, y, "W"}
-  defp move({"L", 180}, {x, y, "S"}), do: {x, y, "N"}
-  defp move({"L", 180}, {x, y, "W"}), do: {x, y, "E"}
+  defp follow_waypoint(instructions) do
+    Enum.reduce(instructions, {{0, 0}, {10, 1}}, &move_by_waypoint/2)
+  end
 
-  defp move({"L", 270}, {x, y, "N"}), do: {x, y, "E"}
-  defp move({"L", 270}, {x, y, "E"}), do: {x, y, "S"}
-  defp move({"L", 270}, {x, y, "S"}), do: {x, y, "W"}
-  defp move({"L", 270}, {x, y, "W"}), do: {x, y, "N"}
+  defp move_by_waypoint({"F", value}, {{x, y}, {wx, wy}}) do
+    {{x + wx * value, y + wy * value}, {wx, wy}}
+  end
+
+  defp move_by_waypoint({"N", value}, {{x, y}, {wx, wy}}), do: {{x, y}, {wx, wy + value}}
+  defp move_by_waypoint({"E", value}, {{x, y}, {wx, wy}}), do: {{x, y}, {wx + value, wy}}
+  defp move_by_waypoint({"S", value}, {{x, y}, {wx, wy}}), do: {{x, y}, {wx, wy - value}}
+  defp move_by_waypoint({"W", value}, {{x, y}, {wx, wy}}), do: {{x, y}, {wx - value, wy}}
+
+  defp move_by_waypoint({"R", 90}, {{x, y}, {wx, wy}}), do: {{x, y}, {wy, -wx}}
+  defp move_by_waypoint({"R", 180}, {{x, y}, {wx, wy}}), do: {{x, y}, {-wx, -wy}}
+  defp move_by_waypoint({"R", 270}, {{x, y}, {wx, wy}}), do: {{x, y}, {-wy, wx}}
+
+  defp move_by_waypoint({"L", value}, state), do: move_by_waypoint({"R", 360 - value}, state)
 
   defp manhattan_distance({x, y, _}), do: abs(x) + abs(y)
+  defp manhattan_distance({{x, y}, _}), do: abs(x) + abs(y)
 end
